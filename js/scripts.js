ffe4b2b466649427a8fe17d2992570846f576843
michaelakiyama <michaelakiyama@gmail.com>
2021-12-14 20:01:44 -0800

    Updated scripts.js
    Updated code to resolve errors found.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
+console.log('Hi! Welcome to my portfolio.')
 
-console.log("Hi! Welcome to my portfolio.")
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive'; 
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
